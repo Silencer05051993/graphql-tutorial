00f941ad76a8df703bd774bd7020ca0fe6deee94
bac-ta <bac-ta@dimage.co.jp>
2019-11-25 14:23:23 +0700

    demo logic user

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_graphql import GraphQLView
+from graphene import Schema
+
+from com.tutorial.schema.user import User
+
+view_func = GraphQLView.as_view('graphql', schema=Schema(query=User))
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+app.add_url_rule('/', view_func=view_func)
 
 if __name__ == '__main__':
     app.run()
